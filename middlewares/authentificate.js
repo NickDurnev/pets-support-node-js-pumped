b1685913fb113eacd9bbe622bc372143fc6f4c56
DmitriyGovosyants <Demiangvs@gmail.com>
2022-12-12 17:22:04 +0200

    fix: invalid token status code

--- middlewares/authentificate.js
+++ middlewares/authentificate.js
@@ -10,21 +10,21 @@ const authentificate = async (req, res, next) => {
 
   try {
     if (bearer !== 'Bearer') {
-      return res.status(401).json({ message: 'Not authorized' });
+      return res.status(498).json({ message: 'Not authorized' });
     }
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (user.token !== token) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return res.status(498).json({ message: 'Invalid token' });
     }
     if (!user || !user.token) {
-      return res.status(401).json({ message: 'Not authorized' });
+      return res.status(498).json({ message: 'Not authorized' });
     }
     req.user = user;
     next();
   } catch (error) {
     if (error.message === 'invalid signature') {
-      error.status = 401;
+      error.status = 498;
     }
     next(error);
   }
